77410cd0981091fa96ead8e7f9a3affa2c01ae74
3ANov <3anovdev@gmail.com>
2022-12-02 19:01:20 +0300

    made searching links by class name

--- pyshop_ozon_parser/spiders/ozon_spider.py
+++ pyshop_ozon_parser/spiders/ozon_spider.py
@@ -1,7 +1,9 @@
 import logging as log
 
 import scrapy
+from scrapy import Selector
 from scrapy_selenium import SeleniumRequest
+from selenium.webdriver.common.by import By
 
 from pyshop_ozon_parser.items import OzonSmartphoneItem
 
@@ -16,15 +18,27 @@ class OzonSpider(scrapy.Spider):
 
     def parse(self, response):
         log.info(response.url)
-        for phones in response.css('div.k5u'):
+        # sel = Selector(response)
+        driver = response.meta['driver']
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.implicitly_wait(5)
+        driver.execute_script("window.scrollTo(document.body.scrollHeight, 0);")
+        phones = driver.find_elements(By.CSS_SELECTOR, '.tile-hover-target')
+        # phones = sel.xpath('//a[@class="tile-hover-target kn7"]')
+        log.info(phones)
+        log.info(len(phones))
+
+        for phone in phones:
             item = OzonSmartphoneItem()
-            item.name = phones.css('a[href*=tile-hover-target.ok9]::text')[0].extract()
-            print(phones.css('a[href*=tile-hover-target.ok9]::text'))
+            item['url'] = phone.get_attribute('href')
 
             # print('Проверка ' + phones.css('tile-hover-target ok9'))
             # yield {
             #     'text': phones.css('span.d3z.z3d.d4z.d6z.tsBodyL.ok9').get(),
             # }
-
-
-
+        # yield SeleniumRequest(
+        #     url=self.start_urls,
+        #     wait_time=1000,
+        #     callback=self.parse,
+        # )
