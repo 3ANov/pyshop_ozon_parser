98af3c19567eb00b14f3a7160bf2bb4088ee4f50
3ANov <3anovdev@gmail.com>
2022-12-12 23:03:41 +0300

    rewrited utility for parsing os info

--- pyshop_ozon_parser/utils.py
+++ pyshop_ozon_parser/utils.py
@@ -19,26 +19,29 @@ def parse_json_find_os_info(input_json_text):
 
     for key, value in json_data['widgetStates'].items():
         if 'webCharacteristics' in key:
-            template_os_name_string = r"\{'key': 'OSWithoutVer', 'name': 'Операционная система', " \
-                                      "'values': \[\{'text': '([^']*)', ('link': '[^']*', )*'key': 'OSWithoutVer_0'\}]\}"
             phone_characteristics_string = str(json.loads(json_data['widgetStates'][key])['characteristics'])
-            # print(phone_characteristics_string)
-            os_name_searched_groups = re.search(template_os_name_string, phone_characteristics_string)
-            if os_name_searched_groups is None:
-                os_name = ''
-            else:
-                os_name = os_name_searched_groups.group(1)
 
-            template_os_version_string = "\{'key': '"+os_name+"Ver([a-zA-Z])*', 'name': '[^']*', 'values': \[\{'text': '([^']*)', ('link': '[^']*', )*'key': '[^']*'\}]\}"
+            template_os_version_string = "\{'key': '(?P<os_ver>((?P<os_name>([^']*))Ver[(sion)|]*))', 'name': '[^']* (?P=os_name)', 'values': \[\{'text': '((?P=os_name)([^']*))', ('link': '[^']*', )*'key': '((?P=os_ver)[^']*)'\}]\}"
 
             os_version_search_groups = re.search(template_os_version_string, phone_characteristics_string)
-            if os_version_search_groups is None:
-                os_version = ''
-            else:
-                os_version = os_version_search_groups.group(2)
 
-            os_info['os_name'] = os_name
-            os_info['os_version'] = os_version
+            template_os_name_string = r"\{'key': 'OSWithoutVer', 'name': 'Операционная система', " \
+                                      "'values': \[\{'text': '([^']*)', ('link': '[^']*', )*'key': 'OSWithoutVer_0'\}]\}"
+
+            os_name_searched_groups = re.search(template_os_name_string, phone_characteristics_string)
+
+            if os_version_search_groups is None and os_name_searched_groups is None:
+                os_info['os_version'] = ''
+                os_info['os_name'] = ''
+            elif os_version_search_groups is None:
+                os_info['os_version'] = ''
+                os_info['os_name'] = os_name_searched_groups.group(1)
+            elif os_name_searched_groups is None:
+                os_info['os_version'] = os_version_search_groups.group(5)
+                os_info['os_name'] = os_version_search_groups.group('os_name')
+            else:
+                os_info['os_name'] = os_name_searched_groups.group(1)
+                os_info['os_version'] = os_version_search_groups.group(5)
 
             break
     return os_info
